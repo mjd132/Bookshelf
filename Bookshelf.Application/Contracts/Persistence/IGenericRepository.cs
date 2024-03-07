7f2f6a600028abf58e0e7da69db8a0fac2c90ef6
mjd132 <1111222233334444accb@gmail.com>
2024-03-08 00:42:46 +0330

    change T to List<T> in get async

--- Bookshelf.Application/Contracts/Persistence/IGenericRepository.cs
+++ Bookshelf.Application/Contracts/Persistence/IGenericRepository.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Bookshelf.Application.Contracts.Persistence;
+﻿namespace Bookshelf.Application.Contracts.Persistence;
 
 public interface IGenericRepository<T> where T : class
 {
-    Task<T> GetAsync();
+    Task<List<T>> GetAsync();
     Task<T> GetByIdAsync(int id);
     Task<T> CreateAsync(T entity);
     Task<T> UpdateAsync(T entity);
