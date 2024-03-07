8b565eaf2b339eed6398ac175d9f9c43d6c8e887
mjd132 <1111222233334444accb@gmail.com>
2024-03-07 20:21:52 +0330

    Add project files.

---
+++ Bookshelf/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookshelf.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
