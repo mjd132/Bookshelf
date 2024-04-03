772aec0043643334920ce67e0a8384a78667a9d3
mjd132 <1111222233334444accb@gmail.com>
2024-04-04 02:47:51 +0330

    Change property name

--- Bookshelf.Application/Features/Publisher/Queries/GetPublisherDetails/PublisherDetailsDto.cs
+++ Bookshelf.Application/Features/Publisher/Queries/GetPublisherDetails/PublisherDetailsDto.cs
@@ -7,7 +7,7 @@ public class PublisherDetailsDto
     public int Id { get; set; }
     public string Name { get; set; }
     public string? WebsiteUrl { get; set; }
-    public ICollection<BookDto>? BooksDto { get; set; }
+    public ICollection<BookDto>? Books { get; set; }
     public DateTime? CreatedAt { get; set; }
     public DateTime? ModifiedAt { get; set; }
     public string? ModifiedBy { get; set; }
