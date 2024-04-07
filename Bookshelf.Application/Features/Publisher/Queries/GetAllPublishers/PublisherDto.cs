974a39c7d25d69fbb55eb09759bf13da98269d43
mjd132 <1111222233334444accb@gmail.com>
2024-04-08 01:48:10 +0330

    Fix error null argument

--- Bookshelf.Application/Features/Publisher/Queries/GetAllPublishers/PublisherDto.cs
+++ Bookshelf.Application/Features/Publisher/Queries/GetAllPublishers/PublisherDto.cs
@@ -3,7 +3,7 @@
 public class PublisherDto
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string? WebsiteUrl { get; set; }
     
 }
