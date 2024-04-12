5d1a8d19497708f94fcc062eadcbfc975d9361a6
mjd132 <1111222233334444accb@gmail.com>
2024-04-13 02:24:27 +0330

    Init BlazorUI Project

---
+++ Bookshelf.BlazorUI/Program.cs
@@ -0,0 +1,19 @@
+using Bookshelf.BlazorUI;
+using Bookshelf.BlazorUI.Contracts;
+using Bookshelf.BlazorUI.Services;
+using Bookshelf.BlazorUI.Services.Base;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:7253"));
+
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IAuthorService, AuthorService>();
+builder.Services.AddScoped<IPublisherService, PublisherService>();
+
+await builder.Build().RunAsync();
