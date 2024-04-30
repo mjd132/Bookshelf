99248afb8251e826481e2c43330a7280524c636c
mjd132 <1111222233334444accb@gmail.com>
2024-05-01 00:01:22 +0330

    Implement DbContext for identity project

---
+++ Bookshelf.Identity/DatabaseContext/BookshelfIdnetityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Bookshelf.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookshelf.Identity.DatabaseContext;
+
+public class BookshelfIdnetityDbContext : IdentityDbContext<ApplicationUser>
+{
+	public BookshelfIdnetityDbContext(DbContextOptions<BookshelfIdnetityDbContext> options):base(options)
+	{
+	
+		
+	}
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+		builder.ApplyConfigurationsFromAssembly(typeof(BookshelfIdnetityDbContext).Assembly);
+    }
+}
